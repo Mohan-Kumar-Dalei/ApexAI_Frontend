448f1c770bf8d5a921623af7e1bbb520dd9bc59c
Mohan Kumar Dalei <mohankumardalei2001@gmail.com>
2025-09-22 19:01:30 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import MainRouter from './router/MainRouter.jsx'
 const App = () => {
   return (
-    <div>
+    <div className='w-screen h-screen bg-gray-950 overflow-hidden'>
       <MainRouter />
     </div>
   )
